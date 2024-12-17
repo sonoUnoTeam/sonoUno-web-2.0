2821aa633b1e2ca98e4acd6b43d98f5f1d5fe142
Johanna Casado <johi.ceh@gmail.com>
2024-12-17 14:06:36 -0300

    testing path

--- sonounoweb/sonounoweb/settings.py
+++ sonounoweb/sonounoweb/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+tp7v=sjir%6@7qa8dc!$g1$%!z!97+ij4&j*9dig5u2cg(jmz
 DEBUG = True
 
 #to UM server
-ALLOWED_HOSTS = ['sonounoapp.um.edu.ar']
+ALLOWED_HOSTS = ['sonounoapp.um.edu.ar', '127.0.0.1']
 
 #Habilita el uso de messages
 MESSAGE_STORAGE= "django.contrib.messages.storage.cookie.CookieStorage"
@@ -127,7 +127,13 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'sonif1D/static'),
+    print('Johi aqui!!'),
+    print(BASE_DIR)
 ]
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
