965bf875a424815a18a9b96b90e84c00a93bda67
Johanna Casado <johi.ceh@gmail.com>
2024-12-18 10:22:15 -0300

    static_root

--- sonounoweb/sonounoweb/settings.py
+++ sonounoweb/sonounoweb/settings.py
@@ -17,11 +17,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-BASE_DIR1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
-print('************************************************')
-print(BASE_DIR1)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -128,6 +123,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'sonif1D/static'),
