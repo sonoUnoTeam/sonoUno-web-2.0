dcc1eecfeb41b7781e5cdedceceaf165ce6c5a54
Johanna Casado <johi.ceh@gmail.com>
2024-12-18 11:56:23 -0300

    test

--- sonounoweb/sonounoweb/settings.py
+++ sonounoweb/sonounoweb/settings.py
@@ -126,11 +126,12 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'sonif1D/static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'sonif1D/static'),
+#]
 
 # Media files
 MEDIA_URL = '/media/'
