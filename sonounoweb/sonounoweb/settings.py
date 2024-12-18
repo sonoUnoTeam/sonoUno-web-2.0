4815b32dbb49c0c7f539cbcb54026f4a38e6182e
Johanna Casado <johi.ceh@gmail.com>
2024-12-18 10:13:22 -0300

    test

--- sonounoweb/sonounoweb/settings.py
+++ sonounoweb/sonounoweb/settings.py
@@ -17,6 +17,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print('************************************************')
+print(BASE_DIR1)
 
 
 # Quick-start development settings - unsuitable for production
