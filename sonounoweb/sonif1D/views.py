0a1b0d9b5a94bca3443db3a673d48903891443bd
Johanna <johi.ceh@gmail.com>
2025-05-25 16:12:46 -0300

    otro print

--- sonounoweb/sonif1D/views.py
+++ sonounoweb/sonif1D/views.py
@@ -280,7 +280,7 @@ def generar_auido_base64(data, request):
         sonido = simpleSound()
         print("Línea 281 al tratar de generar el audio con los siguiente datos:\n",data)
 
-        print(type(data))
+        print(data[:,0])
 
         # Llama al método generate_sound para obtener el sonido generado
         wav_data = sonido.generate_sound(data[:, 0], data[:, 1])  # Usamos x como data_x y y como data_y
