a4ae8126bae48fc2b17060b1149b8fe9e91905b9
Gonzalo Cayo <gonzacayo@gmail.com>
2025-11-19 20:44:26 -0300

    config logger

--- sonounoweb/imagesonif/apps.py
+++ sonounoweb/imagesonif/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ImagesonifConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'imagesonif'
+
+    def ready(self):
+        """Configurar logging cuando la aplicación esté lista."""
+        from .logging_config import setup_imagesonif_logging
+        setup_imagesonif_logging()
